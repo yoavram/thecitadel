7dc6d5efc81eb04fc5f532dd83feca319043334e
Yoav Ram <yoavram+github@gmail.com>
2015-02-19 10:24:40 +0200

    drop db before creating it

--- create_db.py
+++ create_db.py
@@ -1,5 +1,9 @@
 from app import db
+import sys
 
+if raw_input("This will drop all tables in the db, do you want to continue? (y,N)\n") != 'y':
+	sys.exit()
+db.drop_all()
 print "Creating db..."
 db.create_all()
 db.session.commit()
